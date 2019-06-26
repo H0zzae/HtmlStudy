2aa1f4495d14b321fda683996d4243fa9d136300
H0zzae <39540561+H0zzae@users.noreply.github.com>
2019-06-26 19:46:35 +0900

    자바스크립트 기초

---
+++ javascript/01-script/colors.js
@@ -0,0 +1,33 @@
+var Body = {
+	setColor:function (color){
+		document.querySelector('body').style.color = color;
+	},
+	setbackgroundColor:function(color) {
+		document.querySelector('body').style.backgroundColor = color;
+	}
+}
+var Links = {
+	setColor: function(color){
+	var alist = document.querySelectorAll('a');
+	var i = 0;
+	while(i < alist.length) {
+		console.log(alist[i]);
+		alist[i].style.color = color;
+		i = i+1;
+		}
+	}
+}
+function nightDayHandler(self){;
+	var target = document.querySelector('body')
+if(self.value === 'night'){
+	Body.setbackgroundColor('black');
+	Body.setColor('white');
+	self.value = 'day';
+	Links.setColor('yellow');
+} else {
+	Body.setbackgroundColor('white');
+	Body.setColor('black');
+	self.value = 'night';
+	Links.setColor('blue');
+	}
+}
